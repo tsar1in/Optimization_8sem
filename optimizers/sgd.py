ccfa7f272eb7e0d0446cfcbf2a2c47ece9d49c60
Pavel Shapka <shapka.pa@phystech.edu>
2025-05-09 18:39:43 +0300

    3

--- optimizers/sgd.py
+++ optimizers/sgd.py
@@ -1,10 +1,11 @@
 import jax
 import jax.numpy as jnp
 
-from typing import Callable, NamedTuple
+from typing import Callable, NamedTuple, Optional
 
 class SGDState(NamedTuple):
-    batch_stats: dict
+    params: dict
+    batch_stats: dict = {}
 
 class SGD:
     def __init__(self,
@@ -15,21 +16,18 @@ class SGD:
         self.loss_fn = jax.jit(loss_fn) if need_jit else loss_fn
         self.update_fn = jax.jit(self._make_update_fn()) if need_jit else self._make_update_fn()
 
-    def init(self, params: dict) -> SGDState:
-        return SGDState(batch_stats=params["batch_stats"])
+    def init(self, variables: dict) -> SGDState:
+        return SGDState(params=variables["params"], batch_stats=variables.get("batch_stats", {}))
     
     def _make_update_fn(self) -> Callable:
-        def update_fn(params: dict,
-                      batch: tuple[jnp.ndarray, jnp.ndarray],
-                      state: SGDState) -> tuple[dict, SGDState]:
-            (_, new_batch_stats), grads = jax.value_and_grad(lambda p: self.loss_fn({"params": p, "batch_stats": state.batch_stats}, 
-                                              batch), has_aux=True)(params)
-            new_params = jax.tree_util.tree_map(lambda p, g: p - self.lr * g, params, grads)
-            return new_params, SGDState(batch_stats=new_batch_stats)
+        def update_fn(state: SGDState, batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[float, SGDState]:
+            loss_fn = lambda p: self.loss_fn({"params": p, "batch_stats": state.batch_stats}, batch)
+            (loss, new_batch_stats), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+            new_params = jax.tree_util.tree_map(lambda p, g: p - self.lr * g, state.params, grads)
+            return loss, SGDState(params=new_params, batch_stats=new_batch_stats)
         return update_fn
 
     def update(self,
-               params: dict,
-               batch: tuple[jnp.ndarray, jnp.ndarray],
-               state: SGDState) -> tuple[float, tuple[dict, SGDState]]:
-        return self.update_fn(params, batch, state)
+               state: SGDState,
+               batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[float, SGDState]:
+        return self.update_fn(state, batch)
