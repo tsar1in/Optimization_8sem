f828cd383a4af417c1ac86b6ae4541aca53d61a0
Pavel Shapka <shapka.pa@phystech.edu>
2025-05-10 00:05:12 +0300

    4

--- optimizers/page.py
+++ optimizers/page.py
@@ -1,15 +1,21 @@
 import jax
 import jax.numpy as jnp
 
+from flax.core.frozen_dict import FrozenDict
+
 from typing import Any, Callable, NamedTuple
 
 class PAGEState(NamedTuple):
     g: Any
     key: jax.random.PRNGKey
 
+    params: FrozenDict
+    batch_stats: FrozenDict
+
 class PAGE:
     def __init__(self,
                  loss_fn: Callable,
+                 eval_loss_fn: Callable,
                  p: float,
                  lr: float,
                  bs: int,
@@ -20,51 +26,61 @@ class PAGE:
         self.bs = bs
         self.bs_hat = bs_hat
         self.loss_fn = jax.jit(loss_fn) if need_jit else loss_fn
+        self.eval_loss_fn = jax.jit(eval_loss_fn) if need_jit else eval_loss_fn
         self.update_fn = jax.jit(self._make_update_fn()) if need_jit else self._make_update_fn()
         self._compute_full_grad_fn = jax.jit(self._make_full_grad_fn()) if need_jit else self._make_full_grad_fn()
         self._compute_partial_grad_fn = jax.jit(self._make_partial_grad_fn()) if need_jit else self._make_partial_grad_fn()
 
-    def init(self, params: dict, init_batch: tuple[jnp.ndarray, jnp.ndarray]) -> PAGEState:
-        g = self._compute_full_grad_fn(params, init_batch)
-        return PAGEState(g=g, key=jax.random.PRNGKey(0))
+    def init(self, variables: dict, init_batch: tuple[jnp.ndarray, jnp.ndarray]) -> PAGEState:
+        g = self._compute_full_grad_fn(variables, init_batch)
+        return PAGEState(g=g, key=jax.random.PRNGKey(0), params=FrozenDict(variables["params"]), batch_stats=FrozenDict(variables["batch_stats"]))
     
     def _make_full_grad_fn(self) -> Callable:
-        def compute_full_grad(params: dict, batch: tuple[jnp.ndarray, jnp.ndarray]) -> jnp.ndarray:
-            return jax.grad(self.loss_fn)(params, batch)
+        def compute_full_grad(state: PAGEState, batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[float, jnp.ndarray, FrozenDict]:
+            loss_fn = lambda p: self.loss_fn({"params": p, "batch_stats": state.batch_stats}, batch)
+            (loss, new_batch_stats), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+            return loss, grads, FrozenDict(new_batch_stats)
         return compute_full_grad
     
     def _make_partial_grad_fn(self) -> Callable:
-        def compute_partial_grad(new_params: dict, params: dict, batch: tuple[jnp.ndarray, jnp.ndarray]) -> jnp.ndarray:
-            new_f_grad = jax.grad(self.loss_fn)(new_params, batch)
-            old_f_grad = jax.grad(self.loss_fn)(params, batch)
-            return jax.tree_util.tree_map(lambda new, cur: new - cur, new_f_grad, old_f_grad)
+        def compute_partial_grad(new_state: PAGEState, old_state: PAGEState, batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[float, jnp.ndarray, FrozenDict]:
+            loss_fn = lambda p: self.loss_fn({"params": p, "batch_stats": new_state.batch_stats}, batch)
+            (loss, new_batch_stats), new_f_grad = jax.value_and_grad(loss_fn, has_aux=True)(new_state.params)
+
+            loss_fn = lambda p: self.eval_loss_fn({"params": p, "batch_stats": old_state.batch_stats}, batch)
+            old_f_grad = jax.grad(loss_fn)(old_state.params)
+            delta_g = jax.tree_util.tree_map(lambda new, old: new - old, new_f_grad, old_f_grad)
+            return loss, delta_g, FrozenDict(new_batch_stats)
         return compute_partial_grad
     
     def _make_update_fn(self) -> Callable:
-        def update_fn(params: dict, batch: tuple[jnp.ndarray, jnp.ndarray], state: PAGEState) -> tuple[dict, PAGEState]:
+        def update_fn(state: PAGEState, batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[float, PAGEState]:
             key, subkey = jax.random.split(state.key)
-            new_params = jax.tree_util.tree_map(lambda p, g: p - self.lr * g, params, state.g)
+            new_params = jax.tree_util.tree_map(lambda p, g: p - self.lr * g, state.params, state.g)
 
             do_full_update = jax.random.bernoulli(subkey, p=self.p)
 
-            def full_update(batch: tuple[jnp.ndarray, jnp.ndarray]) -> jnp.ndarray:
+            def full_update(batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[jnp.ndarray, FrozenDict]:
                 return self._compute_full_grad_fn(new_params, batch)
             
-            def partial_update(batch: tuple[jnp.ndarray, jnp.ndarray]) -> jnp.ndarray:
+            def partial_update(batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[jnp.ndarray, FrozenDict]:
                 idxs = jax.random.choice(subkey,
                                          jnp.arange(batch[0].shape[0]), 
                                          shape=(self.bs_hat,),
                                          replace=False)
                 sub_batch = (batch[0][idxs], batch[1][idxs])
-                delta_g = self._compute_partial_grad_fn(new_params, params, sub_batch)
-                return jax.tree_util.tree_map(lambda g, dg: g + dg, state.g, delta_g)
+                delta_g, new_batch_stats = self._compute_partial_grad_fn(new_params, state.params, sub_batch)
+                return jax.tree_util.tree_map(lambda g, dg: g + dg, state.g, delta_g), FrozenDict(new_batch_stats)
             
-            new_g = jax.lax.cond(do_full_update,
-                                 full_update,
-                                 partial_update,
-                                 batch)
+            loss, new_g, new_batch_stats = jax.lax.cond(do_full_update,
+                                                        full_update,
+                                                        partial_update,
+                                                        batch)
 
-            return new_params, PAGEState(g=new_g, key=key)
+            return loss, PAGEState(g=new_g,
+                                   key=key,
+                                   params=FrozenDict(new_params),
+                                   batch_stats=FrozenDict(new_batch_stats))
         return update_fn
 
     def update(self,
--- optimizers/sgd.py
+++ optimizers/sgd.py
@@ -1,11 +1,13 @@
 import jax
 import jax.numpy as jnp
 
-from typing import Callable, NamedTuple, Optional
+from flax.core.frozen_dict import FrozenDict
+
+from typing import Callable, NamedTuple
 
 class SGDState(NamedTuple):
-    params: dict
-    batch_stats: dict = {}
+    params: FrozenDict
+    batch_stats: FrozenDict
 
 class SGD:
     def __init__(self,
@@ -17,14 +19,14 @@ class SGD:
         self.update_fn = jax.jit(self._make_update_fn()) if need_jit else self._make_update_fn()
 
     def init(self, variables: dict) -> SGDState:
-        return SGDState(params=variables["params"], batch_stats=variables.get("batch_stats", {}))
+        return SGDState(params=FrozenDict(variables["params"]), batch_stats=FrozenDict(variables.get("batch_stats", {})))
     
     def _make_update_fn(self) -> Callable:
         def update_fn(state: SGDState, batch: tuple[jnp.ndarray, jnp.ndarray]) -> tuple[float, SGDState]:
             loss_fn = lambda p: self.loss_fn({"params": p, "batch_stats": state.batch_stats}, batch)
             (loss, new_batch_stats), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
             new_params = jax.tree_util.tree_map(lambda p, g: p - self.lr * g, state.params, grads)
-            return loss, SGDState(params=new_params, batch_stats=new_batch_stats)
+            return loss, SGDState(params=FrozenDict(new_params), batch_stats=FrozenDict(new_batch_stats))
         return update_fn
 
     def update(self,
